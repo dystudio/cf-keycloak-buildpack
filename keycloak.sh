24764f9c3db82e3d1ad56164e27f9c62a294d3fa
Jose Riguera <jriguera@gmail.com>
2019-10-03 15:07:57 +0200

    initial commit

---
+++ keycloak.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -e
+
+if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
+    $KEYCLOAK_DIR/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
+fi
+
+SYS_PROPS=" -Dkeycloak.hostname.fixed.httpPort=${PORT:-8080}"
+
+if [ "$KEYCLOAK_ALWAYS_HTTPS" != "" ]; then
+    SYS_PROPS+=" -Dkeycloak.hostname.fixed.alwaysHttps=$KEYCLOAK_ALWAYS_HTTPS"
+fi
+
+exec $KEYCLOAK_DIR/bin/standalone.sh -c=standalone-ha.xml $SYS_PROPS $@
+exit $?
---
+++ parameters.sh
@@ -0,0 +1,6 @@
+# Variables
+
+# default versions
+KEYCLOAK_VERSION="${KEYCLOAK_VERSION:-6.0.1}"
+
+KEYCLOAK_DIR="${DEPS_DIR}/${DEPS_IDX}/jboss"
