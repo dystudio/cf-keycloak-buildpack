edab7a494897a4d9fc3f1c6ed1f4096fba9f1068
Jose Riguera <jriguera@gmail.com>
2019-10-03 16:24:28 +0200

    debug and fix

--- keycloak.sh
+++ keycloak.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+echo ">>Creating user"
 if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
     $KEYCLOAK_DIR/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
 fi
@@ -11,5 +12,6 @@ if [ "$KEYCLOAK_ALWAYS_HTTPS" != "" ]; then
     SYS_PROPS+=" -Dkeycloak.hostname.fixed.alwaysHttps=$KEYCLOAK_ALWAYS_HTTPS"
 fi
 
+echo ">>Executing standalone.sh -c=standalone-ha.xml $SYS_PROPS $@"
 exec $KEYCLOAK_DIR/bin/standalone.sh -c=standalone-ha.xml $SYS_PROPS $@
 exit $?
