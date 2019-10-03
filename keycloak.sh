7ecb9021f2f1af4793fa5cd8f73654f4d22c55cb
Jose Riguera <jriguera@gmail.com>
2019-10-03 17:06:28 +0200

    fix startup

--- keycloak.sh
+++ keycloak.sh
@@ -5,13 +5,14 @@ echo ">>Creating user"
 if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
     $KEYCLOAK_DIR/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
 fi
+# -Dkeycloak.hostname.fixed.httpPort=${PORT:-8080} 
+#SYS_PROPS=" -Djboss.bind.address=0.0.0.0 -Djboss.bind.address.private=0.0.0.0"
+SYS_PROPS=" "
 
-SYS_PROPS=" -Dkeycloak.hostname.fixed.httpPort=${PORT:-8080} -Djboss.bind.address=0.0.0.0 -Djboss.bind.address.private=0.0.0.0"
-
-if [ "$KEYCLOAK_ALWAYS_HTTPS" != "" ]; then
-    SYS_PROPS+=" -Dkeycloak.hostname.fixed.alwaysHttps=$KEYCLOAK_ALWAYS_HTTPS"
-fi
+# if [ "$KEYCLOAK_ALWAYS_HTTPS" != "" ]; then
+#     SYS_PROPS+=" -Dkeycloak.hostname.fixed.alwaysHttps=$KEYCLOAK_ALWAYS_HTTPS"
+# fi
 
 echo ">>Executing standalone.sh -c=standalone-ha.xml $SYS_PROPS $@"
-exec $KEYCLOAK_DIR/bin/standalone.sh -c=standalone-ha.xml $SYS_PROPS $@
+exec $KEYCLOAK_DIR/bin/standalone.sh -c=standalone-ha.xml $SYS_PROPS -b 0.0.0.0
 exit $?
