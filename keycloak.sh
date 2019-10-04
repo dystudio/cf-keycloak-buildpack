0d278b9b69f4a1c3ca8559a3e05d4bb8508754c5
Gerard Laan <prolane.org@gmail.com>
2019-10-04 12:15:13 +0200

    JGroups Discovery added

--- keycloak.sh
+++ keycloak.sh
@@ -13,6 +13,19 @@ SYS_PROPS=" -Dkeycloak.hostname.fixed.alwaysHttps=false"
 #     SYS_PROPS+=" -Dkeycloak.hostname.fixed.alwaysHttps=$KEYCLOAK_ALWAYS_HTTPS"
 # fi
 
+# JGroups for cluster discovery
+if [ $JGROUPS_DISCOVERY_PROTOCOL ]; then
+    echo ">>Set JGroups Discovery"
+    
+    JGROUPS_DISCOVERY_PROPERTIES_PARSED=`echo $JGROUPS_DISCOVERY_PROPERTIES | sed "s/=/=>/g"`
+    echo ">>>Setting JGroups discovery to $JGROUPS_DISCOVERY_PROTOCOL with properties $JGROUPS_DISCOVERY_PROPERTIES_PARSED"
+    echo "set keycloak_jgroups_discovery_protocol=${JGROUPS_DISCOVERY_PROTOCOL}" >> "$KEYCLOAK_DIR/bin/.jbossclirc"
+    echo "set keycloak_jgroups_discovery_protocol_properties=${JGROUPS_DISCOVERY_PROPERTIES_PARSED}" >> "$KEYCLOAK_DIR/bin/.jbossclirc"
+    echo "set keycloak_jgroups_transport_stack=${JGROUPS_TRANSPORT_STACK:-tcp}" >> "$KEYCLOAK_DIR/bin/.jbossclirc"
+    
+    $KEYCLOAK_DIR/bin/jboss-cli.sh --file="/opt/jboss/tools/cli/jgroups/discovery/default.cli" >& /dev/null
+fi
+
 echo ">>Executing standalone.sh -c=standalone-ha.xml $SYS_PROPS $@"
 exec $KEYCLOAK_DIR/bin/standalone.sh -c=standalone-ha.xml $SYS_PROPS -b 0.0.0.0
 exit $?
