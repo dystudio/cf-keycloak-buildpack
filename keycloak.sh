cfdb576ae3004a29483715834b5f58ce6267174e
Gerard Laan <prolane.org@gmail.com>
2019-10-08 13:53:14 +0200

    Remove undeeded comments

--- keycloak.sh
+++ keycloak.sh
@@ -5,13 +5,10 @@ echo ">>Creating user"
 if [ $KEYCLOAK_USER ] && [ $KEYCLOAK_PASSWORD ]; then
     $KEYCLOAK_DIR/bin/add-user-keycloak.sh --user $KEYCLOAK_USER --password $KEYCLOAK_PASSWORD
 fi
-# -Dkeycloak.hostname.fixed.httpPort=${PORT:-8080} 
-#SYS_PROPS=" -Djboss.bind.address=0.0.0.0 -Djboss.bind.address.private=0.0.0.0"
-SYS_PROPS=" -Dkeycloak.hostname.fixed.alwaysHttps=false"
 
-# if [ "$KEYCLOAK_ALWAYS_HTTPS" != "" ]; then
-#     SYS_PROPS+=" -Dkeycloak.hostname.fixed.alwaysHttps=$KEYCLOAK_ALWAYS_HTTPS"
-# fi
+# -Dkeycloak.hostname.fixed.httpPort=${PORT:-8080} 
+
+SYS_PROPS=" -Dkeycloak.hostname.fixed.alwaysHttps=false"
 
 
 ########################
